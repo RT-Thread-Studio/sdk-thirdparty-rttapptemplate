8899dff6a1d334990f01cc28ec5d8d2f7e72542b
jiangjianbo <jiangjianbo@rt-thread.com>
2023-07-05 15:16:24 +0800

    add rtt app template to sdk third party

---
+++ projects/helloworld_demo/application/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int count1 = 0;
+    int count2 = 10;
+    while (1)
+    {
+        count1++;
+        count2++;
+    }
+
+    return count1+count2;
+}
